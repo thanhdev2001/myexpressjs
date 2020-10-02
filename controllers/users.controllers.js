cbc70f92c6f8f68726d17c2e4316a058df63345e
Nguyễn Chí Thành <classic.nct@gmail.com>
2020-10-02 11:39:50 +0700

    Add uploading feature of avatar

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -1,6 +1,12 @@
 var db = require('../db')
 const shortid = require('shortid')
 
+const process = require('process');
+const fs = require('fs')
+const path = process.cwd() + '/public/'
+
+const defaultAvatar = 'C:\\Users\\ABC\\ExpressJS/public/images/avatar_user2.jpg'
+
 module.exports.index = function(req, res) {
     res.render('index', {
       users: db.get('users').value()
@@ -38,7 +44,20 @@ module.exports.getId = function(req, res){
   }
 
 module.exports.remove = function(req, res){
-    var id = req.params.id
+    var id = req.params.id  
+    var user = db.get('users').find({ id: id}).value()
+  
+    var imgPath = path + user.avatar
+
+    if(imgPath !== defaultAvatar){
+      fs.unlink(imgPath, (err) => {
+        if (err) {
+          console.error(err)
+          return
+        }
+      })
+    }
+    
     db.get('users').remove({ id: id }).write()
 
     res.render('list/remove', {
@@ -55,6 +74,22 @@ module.exports.RefreshRemove = function(req, res){
 module.exports.PostCreate = function(req, res){
     req.body.id = shortid.generate()
     
+    if(req.file === undefined){
+      req.body.avatar = "images/avatar_user2.jpg"
+    }
+    else{
+      req.body.avatar = req.file.path.split('\\').slice(1).join("/")
+    }
+       
     db.get('users').push(req.body).write()
     res.redirect('/users')
   }
+
+module.exports.PostView = function(req, res){
+  var id = req.params.id
+  req.body.avatar = req.file.path.split('\\').slice(1).join("/")
+  
+  db.get('users').find({ id: id}).assign({ avatar: req.body.avatar}).write();
+  
+  res.redirect('/users')
+}
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const port = 3000
@@ -7,18 +9,23 @@ var authController = require('./controllers/auth.controller')
 var db = require('./db')
 const shortid = require('shortid')
 
+var multer  = require('multer')
+var upload = multer({ dest: 'public/uploads/' })
+
 var validate = require('./validate/user.validate')
 
 var authMiddleware = require('./middlewares/auth.middleware')
 
 var cookieParser = require('cookie-parser')
-app.use(cookieParser('abc'))
+app.use(cookieParser(process.env.SESSION_SECRET))
 
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.set('view engine', 'pug')
 app.set('views', './views')
+
+console.log(process.env.SESSION_SECRET)
 
 app.use(express.static('public'))
 
@@ -38,7 +45,9 @@ app.get('/users/remove', authMiddleware.requireAuth, controller.RefreshRemove)
 
 app.get('/users/remove/id/:id', controller.remove)
 
-app.post('/users/create', validate.PostCreate, controller.PostCreate)
+app.post('/users/create', upload.single('avatar'), validate.PostCreate, controller.PostCreate)
+
+app.post('/users/id/:id', upload.single('avatar'), controller.PostView)
 
 app.post('/auth/login', authController.postLogin)
 
